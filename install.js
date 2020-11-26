5cab5518cfa39d7c54b231be21e7531128fbb538
julianoosmir <60453624+julianoosmir@users.noreply.github.com>
2020-11-26 17:01:43 -0300

    Add files via upload

---
+++ install.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+window.onload = () => {
+    let deferredInstallPrompt = null;
+
+    window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+    document.getElementById('btnInstall').addEventListener('click', installPWA);
+
+    window.addEventListener('appinstalled', logAppInstalled);
+
+    function saveBeforeInstallPromptEvent(evt) {
+        deferredInstallPrompt = evt;
+        installButton.removeAttribute('hidden');
+    }
+
+    function installPWA(evt) {
+        deferredInstallPrompt.prompt();
+        evt.srcElement.setAttribute('hidden', true);
+    }
+
+    function logAppInstalled(evt) {
+        console.log('ProjetoDDM App was installed.', evt);
+    }
+
+}
